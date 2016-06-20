ef4cc3678ae4b8e8fcbc804341ab42344ad8deca
Flako <felipeadelgadog@gmail.com>
2016-06-20 02:25:58 -0400

    Arreglo del Kgazo

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Green-Rock</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="/css/style.css">
+        
+    </head>
+
+    <body>
+        
+        @yield('content')
+
+    </body>
+</html>
---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+
+    <h1>WELCOME PAGE</h1>
+
+@stop
